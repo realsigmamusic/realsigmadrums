dfb424c0b951b909e29ad2d430f33dc2f3ec2f1d
samuelsantanaoficial <psicopatadamusica@gmail.com>
2025-11-08 20:24:41 -0300

    Add crash 1

--- realsigmadrums.cpp
+++ realsigmadrums.cpp
@@ -255,6 +255,16 @@ struct MyDrumKit {
 				add_to_rr_group_path(41, path.c_str(), 13, true);
 			}
 
+			// Crash 1
+			for (int i = 1; i <= 7; ++i) {
+				std::string path = base + "/crash1_overhead_v7_r" + std::to_string(i) + ".wav";
+				add_to_rr_group_path(49, path.c_str(), 11, true);
+			}
+			for (int i = 1; i <= 7; ++i) {
+				std::string path = base + "/crash1_room_v7_r" + std::to_string(i) + ".wav";
+				add_to_rr_group_path(49, path.c_str(), 13, true);
+			}
+
 			fprintf(stderr, "MyDrumKit: %zu notas carregadas (CLAP)\n", rr_groups.size());
 		} catch (...) {
 			fprintf(stderr, "MyDrumKit: Erro inesperado ao carregar samples\n");
